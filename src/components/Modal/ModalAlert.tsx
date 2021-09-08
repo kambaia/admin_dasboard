1ac5a8c19162dd3f0b603f74faf8be08d5ce9821
Kambaia ALberto <kambaiaalberto@gmail.com>
2021-09-08 14:46:36 +0100

    addd

--- src/components/Modal/ModalAlert.tsx
+++ src/components/Modal/ModalAlert.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import colors from "../../styles/colors";
 export interface Props {
   open?: boolean;
   setShowFindArtist?: (newValue: boolean) => void;
@@ -7,18 +8,23 @@ export interface Props {
   setClearForm?: (newValue: boolean) => void;
   width?: number | any;
   heigth?: number | any;
-  loading?: boolean
+  loading?: boolean,
 }
 
 export const Modal: React.FC<Props> = ({
   children,
   loading = true,
+  width= 40,
   ...props
 }) => {
   return (
     <FullScreenModal>
-        <Close></Close>
+         <div className="card-modal" style={{width: `${width}%`}}>
+         <Close>
+              <div className="close"></div>
+         </Close>
             {children}
+         </div>
     </FullScreenModal>
   );
 };
@@ -27,13 +33,58 @@ const FullScreenModal = styled.div`
     width: 100vw;
     height:100vh;
     background-color: rgba(0,0,0,0.8);
-    position: absolute;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     left: 0;
     right: 0;
     top: 0;
     z-index: 9999999;
+
+    .card-modal{
+      background-color: #fff;
+      height: auto;
+      position: relative;
+      
+    }
 `
 const Close = styled.div`
-    width: 100vw;
-    height:2vh;
+    width: 100%;
+    border-bottom: 1px solid ${colors.orange};
+    position: absolute;
+    height: 40px;
+    display: flex;
+    align-items:center;
+    justify-content: flex-end;
+    font-size: 25px;
+    .close{
+        background-color: transparent;
+        outline: none;
+        height: 35px;
+        width: 35px;
+        border:none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        opacity: 1;
+        cursor: pointer;
+
+        &:before, &:after{
+            content: '';
+            position: absolute;
+            width: 2px;
+            height: 20px;
+            background-color: #000;
+        }
+        &:before{
+            transform: rotate(45deg);
+        }
+        &:after{
+            transform: rotate(-45deg);
+        }
+        :hover{
+          opacity:.5;
+        }
+    }
 `
--- src/pages/ClassRoom.tsx
+++ src/pages/ClassRoom.tsx
@@ -4,6 +4,8 @@ import { BiBookReader } from "react-icons/bi";
 import bainner from "../assets/bainner.svg";
 import { Link } from "react-router-dom";
 import { db, storage } from "../firebase/config";
+import { FaYoutube } from "react-icons/fa";
+import { FiLink2, FiUpload } from "react-icons/fi";
 import {
   MainContent,
   Container,
@@ -12,85 +14,160 @@ import {
 } from "../styles/pages/generalStyles";
 import CardTeamClass from "../components/CardTeamClass";
 import CardMyQuestion from "../components/CardMyQuestion";
-import { CardTeams, CardMyQuestions } from "../styles/pages/ClassRoomStyles";
+import { CardTeams, CardMyQuestions, ContentAddQuestions } from "../styles/pages/ClassRoomStyles";
 import { Button } from "../components/Button";
 import { useEffect } from "react";
-import { getOnlyMyQuestion, getQuestion } from "../services/classRoomServer";
+import { getOnlyMyQuestion, getQuestion,addQuestion } from "../services/classRoomServer";
 import { getUserProfile } from "../utils";
-import { ModalLoand } from "../components/Modal";
+import { QuestionTypes } from "../types/quesionTypes";
+
 import { Modal } from "../components/Modal/ModalAlert";
 const ClassRoom = () => {
-      const [question, setQuestion] = useState<any>();
-      const [myQuestion, setMyQuestion] = useState<any>()
-    
-   useEffect(()=>{
+  const [question, setQuestion] = useState<any>();
+  const [myQuestion, setMyQuestion] = useState<any>()
+  const [formQ, setFormQ] = useState<QuestionTypes>({
+    idUser: '',
+     question:"",
+     about:'',
+     avatar:'',
+     author:'',
+     creatAt: new Date(),
+    })
+
+  useEffect(() => {
     allQuestion();
     allMyQuestion();
-   },[])
- 
-   const allQuestion= async()=>{
-      const data: any = await getQuestion(getUserProfile()?.uid);
-      if(data){
-        console.log(data)
-        setQuestion(data)
-      }
-   }
-   const allMyQuestion= async()=>{
+  }, [])
+
+  const allQuestion = async () => {
+    const data: any = await getQuestion(getUserProfile()?.uid);
+    if (data) {
+      console.log(data)
+      setQuestion(data)
+    }
+  }
+  const allMyQuestion = async () => {
     const data: any = await getOnlyMyQuestion(getUserProfile()?.uid);
-    if(data){
+    if (data) {
       setMyQuestion(data)
-        console.log("Há nenhum dado desposivel")
-    }else{
+      console.log("Há nenhum dado desposivel")
+    } else {
       console.log("Não há nenhum dado desposivel")
     }
- }
+  }
 
-  
+
+  const onSubmitQuestion = async(e:any)=>{
+    e.preventDefault();
+    const questionData:QuestionTypes = {
+      author: getUserProfile()?.displayName,
+      avatar: getUserProfile()?.photoURL,
+      about: formQ?.about,
+      question:formQ?.question,
+      idUser: getUserProfile()?.uid,
+      creatAt: new Date(),
+      }
+      const res= await addQuestion(getUserProfile()?.uid, questionData);
+}
+
+
   return (
     <>
-    <MainContent>
-      <Container>
-        <CardHeader style={{display:'flex', justifyContent:'space-between'}}>
-           <div>
-           <h2>Todas as salas disponíveis</h2>
-           </div>
-             <Button><Link to="/room">Criar a sua sala</Link></Button>
-          
-        </CardHeader>
-        <Wrapper>
-          <CardTeams>
-            <div className="teams">
-            {
-                    question?.map((item:any, index:number)=>
-                    <CardTeamClass key={index} items={item}/>
-                    
-                    )
-                  }
+      <MainContent>
+        <Container>
+          <CardHeader style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div>
+              <h2>Todas as salas disponíveis</h2>
             </div>
-          </CardTeams>
-       
-        </Wrapper>
-          
-        <Wrapper>
-             <div>
-                  <h2>Perguntas recentes feitas por mim</h2>
-             </div>
-        </Wrapper>
-        <Wrapper>
+            <Button><Link to="/room">Criar a sua sala</Link></Button>
+
+          </CardHeader>
+          <Wrapper>
+            <CardTeams>
+              <div className="teams">
+                {
+                  question?.map((item: any, index: number) =>
+                    <CardTeamClass key={index} items={item} />
+
+                  )
+                }
+              </div>
+            </CardTeams>
+
+          </Wrapper>
+
+          <Wrapper>
+            <div>
+              <h2>Perguntas recentes feitas por mim</h2>
+            </div>
+          </Wrapper>
+          <Wrapper>
             <CardMyQuestions>
-            <div className="my-question">
-                  {
-                    myQuestion?.map((item:any, index:number)=>
-                    <CardMyQuestion items={item}/>
-                    )
-                  }
-             </div>
+              <div className="my-question">
+                {
+                  myQuestion?.map((item: any, index: number) =>
+                    <CardMyQuestion items={item} />
+                  )
+                }
+              </div>
             </CardMyQuestions>
-        </Wrapper>
-      </Container>
-    </MainContent>
-   <Modal/>
-   </>
+          </Wrapper>
+        </Container>
+      </MainContent>
+      <Modal>
+        <ContentAddQuestions>
+        <section id="section-home">
+          <div className="header-questions">
+            <h2>Faça aqui a questão para debateres com o pessoal</h2>
+            <div className="form-questions">
+              <form onSubmit={onSubmitQuestion}>
+                <label htmlFor="" className="sr-only">
+                  Selecione o questão
+                </label>
+                <select onChange={(e) => setFormQ({ ...formQ, about: e.target.value })}>
+                  <option>Questão Relacionada com?</option>
+                  <option>Matemática</option>
+                  <option>Física</option>
+                  <option>Programação</option>
+                  <option>Lingua Portuguesa</option>
+                  <option>Inglês</option>
+                  <option>Francês</option>
+                  <option>Direito</option>
+                  <option>Economia</option>
+                  <option>Outros sabres</option>
+
+
+                </select>
+
+                <label htmlFor="" className="sr-only">
+                  O que é a questão número 1
+                </label>
+                <textarea placeholder="Digite aqui a questão do grupo em questão" onChange={(e) => setFormQ({ ...formQ, question: e.target.value })}></textarea>
+                <footer>
+                  <div className="btn-question">
+                    <span>...</span>
+                    <Button onSubmit={onSubmitQuestion}>Atribuir o desafio</Button>
+                  </div>
+                </footer>
+              </form>
+            </div>
+            <div className="question-footer">
+              <div>
+                <span><FiUpload /></span>
+              </div>
+              <div>
+                <span><FaYoutube /></span>
+              </div>
+              <div>
+                <span><FiLink2 /></span>
+              </div>
+            </div>
+          </div>
+        </section>
+        </ContentAddQuestions>
+      </Modal>
+   
+    </>
   );
 };
 
